0a6d4f64cf4f6b5cf61f56c4cb29bfd19a1296f8
Bonginkosi Malinga <u18312617@tuks.co.za>
2023-02-17 18:24:52 -0800

    task 0 fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,29 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
+ *
+ * This program will assign a random number to the variable n each time
+ * it is executed. Complete the source code in order to print whether the
+ * number stored in the variable n is positive or negative.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+        int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    printf("%d ", n);
-    if (n > 0)
-    {
-        printf("is positive\n");
-    }
-    else if (n == 0)
-    {
-        printf("is zero\n");
-    }
-    else
-    {
-        printf("is negative\n");
-    }
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-    return (0);
+        if (n > 0)
+                printf("%d is positive\n", n);
+        else if (n == 0)
+                printf("%d is zero\n", n);
+        else
+                printf("%d is negative\n", n);
+
+        return (0);
 }
