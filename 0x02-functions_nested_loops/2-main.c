c587f8e3b4bc29b16a541e4c23391dcbca2c2907
Bonginkosi <u18312617@tuks.co.za>
2023-02-21 05:20:21 -0800

    task 2

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Check the code
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-    print_alphabet();
+    print_alphabet_x10();
     return (0);
 }
