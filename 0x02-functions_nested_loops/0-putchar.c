20b1c8e374db84021b713b4c2e1dcbd7634f2aec
Bonginkosi <u18312617@tuks.co.za>
2023-02-21 04:40:24 -0800

    task 0 fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char str[] = "_putchar\n";
     int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
+    for (i = 0; i < 10; i++)
+    {
+        _putchar(i + '0'); // use _putchar function to print the digits 0-9
+    }
+
+    _putchar('\n'); // add a newline character at the end of the output
 
     return (0);
 }
