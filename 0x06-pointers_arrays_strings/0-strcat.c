923387b481a56dad2d60190d556af02478a28a7e
Bonginkosi <u18312617@tuks.co.za>
2023-03-02 01:49:32 -0800

    Task 0 fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcat - concatenates two strings
+ * @dest: distination
+ * @src: source
+ *Return: Always 0
+ */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
-
-    while (dest[i] != '\0')
-        i++;
-
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+strcat(dest, src);
+return (dest);
+}
