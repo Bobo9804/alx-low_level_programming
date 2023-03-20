c6362a15c1d31e8234755902f0d81c7914819f17
Bonginkosi <u18312617@tuks.co.za>
2023-03-20 13:25:41 -0700

    task o fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-ifndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -11,9 +11,9 @@ ifndef DOG_H
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 /**
@@ -26,4 +26,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
+
