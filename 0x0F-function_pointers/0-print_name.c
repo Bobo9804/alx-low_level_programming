529001f6f9a15649e36024bde1750f87ebbb4592
Bonginkosi <u18312617@tuks.co.za>
2023-03-22 21:40:31 -0700

    task 0 fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,10 @@
-/* File: print_name.c */
-
-#include "function_pointers.h"
-
 /**
- * print_name - Prints a name
- * @name: Name to be printed
- * @f: Function pointer to a function that takes a char* as argument and returns void
- *
- * Return: void
+ * print_name - prints name
+ * @name: name to print
+ * @f: function that prints name
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+	if (f)
+		f(name);
 }
